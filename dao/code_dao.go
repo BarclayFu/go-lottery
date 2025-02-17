58b5986faa926f5ae9d9ca8f6e031e7cf17313a8
BarclayFu <50268764+BarclayFu@users.noreply.github.com>
2025-02-17 23:39:36 +0800

    feat: 创建Code DAO层

--- dao/code_dao.go
+++ dao/code_dao.go
@@ -6,17 +6,17 @@ import (
 	"xorm.io/xorm"
 )
 
-type GiftDAO struct {
+type CodeDAO struct {
 	engine *xorm.Engine
 }
 
-func NewGiftDAO(engine *xorm.Engine) *GiftDAO {
-	return &GiftDAO{
+func NewCodeDAO(engine *xorm.Engine) *CodeDAO {
+	return &CodeDAO{
 		engine: engine,
 	}
 }
 
-func (d *GiftDAO) Get(id uint) *models.LtGift {
+func (d *CodeDAO) Get(id uint) *models.LtGift {
 	data := &models.LtGift{Id: id}
 	ok, err := d.engine.Get(data)
 	if ok && err == nil {
@@ -27,11 +27,10 @@ func (d *GiftDAO) Get(id uint) *models.LtGift {
 	}
 }
 
-func (d *GiftDAO) GetAll() []models.LtGift {
+func (d *CodeDAO) GetAll() []models.LtGift {
 	dataList := make([]models.LtGift, 0)
 	err := d.engine.
-		Asc("sys_status").
-		Asc("displayorder").
+		Desc("id").
 		Find(&dataList)
 	if err != nil {
 		log.Println("dao.GetAll err:", err)
@@ -40,7 +39,7 @@ func (d *GiftDAO) GetAll() []models.LtGift {
 	return dataList
 }
 
-func (d *GiftDAO) CountAll() int64 {
+func (d *CodeDAO) CountAll() int64 {
 	count, err := d.engine.Count(&models.LtGift{})
 	if err != nil {
 		return 0
@@ -49,18 +48,18 @@ func (d *GiftDAO) CountAll() int64 {
 	}
 }
 
-func (d *GiftDAO) Delete(id uint) error {
+func (d *CodeDAO) Delete(id uint) error {
 	data := &models.LtGift{Id: id, SysStatus: 1}
 	_, err := d.engine.ID(data.Id).Update(data)
 	return err
 }
 
-func (d *GiftDAO) Update(data *models.LtGift, columns []string) error {
+func (d *CodeDAO) Update(data *models.LtGift, columns []string) error {
 	_, err := d.engine.ID(data.Id).MustCols(columns...).Update(data)
 	return err
 }
 
-func (d *GiftDAO) Create(data *models.LtGift) error {
+func (d *CodeDAO) Create(data *models.LtGift) error {
 	_, err := d.engine.Insert(data)
 	return err
 }
