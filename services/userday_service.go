04d3f7450e26873a6af3abbb00d537db74c57098
BarclayFu <50268764+BarclayFu@users.noreply.github.com>
2025-03-02 15:45:52 +0800

    feat: 创建userday Service

---
+++ services/userday_service.go
@@ -0,0 +1,60 @@
+package services
+
+import (
+	"go-lottery/dao"
+	"go-lottery/models"
+	"xorm.io/xorm"
+)
+
+type UserdayService interface {
+	Get(id uint) *models.LtUserday
+	GetAll(page, size int) []models.LtUserday
+	CountAll() int64
+	Search(uid, day int) []models.LtUserday
+	Count(uid, day uint) uint
+	Delete(id uint) error
+	Update(data *models.LtUserday, columns []string) error
+	Create(data *models.LtUserday) error
+}
+
+type userdayService struct {
+	dao *dao.UserdayDao
+}
+
+func NewUserdayService(engine *xorm.Engine) UserdayService {
+	return &userdayService{
+		dao: dao.NewUserdayDao(engine),
+	}
+}
+
+func (s *userdayService) Get(id uint) *models.LtUserday {
+	return s.dao.Get(id)
+}
+
+func (s *userdayService) GetAll(page, size int) []models.LtUserday {
+	return s.dao.GetAll(page, size)
+}
+
+func (s *userdayService) CountAll() int64 {
+	return s.dao.CountAll()
+}
+
+func (s *userdayService) Search(uid, day int) []models.LtUserday {
+	return s.dao.Search(uid, day)
+}
+
+func (s *userdayService) Count(uid, day uint) uint {
+	return s.dao.Count(uid, day)
+}
+
+func (s *userdayService) Delete(id uint) error {
+	return s.dao.Delete(id)
+}
+
+func (s *userdayService) Update(data *models.LtUserday, columns []string) error {
+	return s.dao.Update(data, columns)
+}
+
+func (s *userdayService) Create(data *models.LtUserday) error {
+	return s.dao.Create(data)
+}
